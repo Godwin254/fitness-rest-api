0e465e13ac21778626e477e9d4eb2d08268de4dc
Godwin254 <godwinbee15@gmail.com>
2022-08-05 15:52:51 +0300

    add service layer && create connection between the service and controller

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,22 +1,39 @@
 
+// initialize the service 
+const workoutService = require('../services/workoutService');
 // handle workout creation
 const getAllWorkouts = (req, res) => {
+
+    // get all workouts from the service layer
+    const allWorkouts = workoutService.getAllWorkouts();
     res.send("GET all workouts");
 }
 
 const getSingleWorkout = (req, res) => {
+
+    // get single workout from the service layer
+    const singleWorkout = workoutService.getSingleWorkout();
     res.send(`GET single workout`);
 }
 
 const createNewWorkout = (req, res) => {
+
+    // create new workout in the service layer
+    const newWorkout = workoutService.createNewWorkout();
     res.send("Create new workout");
 }
 
 const updateSingleWorkout = (req, res) => {
+
+    // update single workout in the service layer
+    const updatedWorkout = workoutService.updateSingleWorkout();
     res.send(`Update single existing workout`);
 }
 
 const deleteSingleWorkout = (req, res) => {
+
+    // delete single workout in the service layer
+    workoutService.deleteSingleWorkout();
     res.send(`Delete single existing workout`);
 }
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,30 @@
+// service layer for workout service - workoutService.js - this is the service layer that will be used by the controller to interact with the database and the model layer to interact with the database 
+
+// service method to get all workouts (business logic)
+const getAllWorkouts = () => {
+    return;
+}
+
+const getSingleWorkout = () => {
+    return;
+}
+
+const createNewWorkout = () => {
+    return;
+}
+
+const updateSingleWorkout = () => {
+    return;
+}
+
+const deleteSingleWorkout = () => {
+    return;
+}
+
+module.exports = {
+    getAllWorkouts,
+    getSingleWorkout,
+    createNewWorkout,
+    updateSingleWorkout,
+    deleteSingleWorkout
+}
