15ba5391198064be6b13dd6ec5ba691551077505
Godwin254 <godwinbee15@gmail.com>
2022-08-05 14:33:18 +0300

    versioning in express api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const { application } = require('express');
 const express = require('express');
+
+const v1Router = require('./v1/routes'); 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || `http://localhost:${PORT}`;
 
 const app = express();
 
-app.get('/gym-api', (req, res) => {
-    res.send("<h1>Server Started Successfully</h1>");
-})
+app.use('/api/v1', v1Router);
 
-app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`App is running on: ${HOST}\n\nlistening on port: ${PORT}`));
