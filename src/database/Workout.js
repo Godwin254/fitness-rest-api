13f3da9e01a35a5788860fdc20955f61495c59b8
Godwin254 <godwinbee15@gmail.com>
2022-08-05 20:13:05 +0300

    add db handler for updating a single data

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -35,4 +35,29 @@ const createNewWorkout = (newWorkout) => {
     return newWorkout;
 }
 
+//update a single workout
+const updateSingleWorkout = (workoutId, changes) => {
+    //check if index exists
+    const indexForUpdate = DB.workouts.findIndex(workout => workout.id === workoutId);
+
+    //if index doesn't exist -1 
+    if (indexForUpdate === -1) {
+        throw new Error('Workout does not exist');
+        return;
+    }
+
+    //create object of the updated workout
+    const updatedWorkout = {
+        ...DB.workouts[indexForUpdate],
+        ...changes,
+        updatedAt: new Date().toDateString("en-US", {timeZone: "Africa/Nairobi"})
+    };
+
+    DB.workouts[indexForUpdate] = updatedWorkout;
+
+    saveToDatabase(DB);
+    return updatedWorkout
+
+}
+
 module.exports = { getAllWorkouts, createNewWorkout };
