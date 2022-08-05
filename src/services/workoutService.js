4d3e713b3cc85f4c1548f3c818e8245a29aaaf08
Godwin254 <godwinbee15@gmail.com>
2022-08-05 19:01:08 +0300

    modify request body at the service layer

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,11 +1,12 @@
 
+const { v4: uuid } = require('uuid');
 const Workout = require('../database/Workout');
 // service layer for workout service - workoutService.js - this is the service layer that will be used by the controller to interact with the database and the model layer to interact with the database 
 
 // service method to get all workouts (business logic)
 const getAllWorkouts = () => {
     const allWorkouts = Workout.getAllWorkouts();
-    
+
     return allWorkouts;
 }
 
@@ -13,8 +14,19 @@ const getSingleWorkout = () => {
     return;
 }
 
-const createNewWorkout = () => {
-    return;
+const createNewWorkout = (newWorkout) => {
+
+    //bloc
+    const workoutToInsert = {
+        ...newWorkout,
+        id: uuid(),
+        createdAt: new Date().toLocaleDateString("en-US", {timeZone: "Africa/Nairobi"}),
+        updatedAt: new Date().toLocaleDateString("en-US", {timeZone: "Africa/Nairobi"})
+    };
+
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+
+    return createdWorkout;
 }
 
 const updateSingleWorkout = () => {
