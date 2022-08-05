ec927f02ecc75561cc9be534ef6565fa3b05eec1
Godwin254 <godwinbee15@gmail.com>
2022-08-05 18:38:36 +0300

    add validation before saving to DB

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,8 +1,27 @@
 
 const DB = require('./db.json');
+const { saveToDatabase } = require('../database/utils');
 
+//accessing all workout data
 const getAllWorkouts = ( ) => {
     return DB.workouts;
+}
+
+//create new workout to the DB
+const createNewWorkout = (newWorkout) => {
+    
+    //check if already added
+    const workoutExists = DB.workouts.findIndex(workout => workout.name === newWorkout.name) > -1;
+
+    if (workoutExists) {
+        throw new Error('Workout already exists');
+        return;
+    }
+
+    DB.workouts.push(newWorkout);
+    saveToDatabase(DB);
+
+    return newWorkout;
 }
 
 module.exports = { getAllWorkouts };
