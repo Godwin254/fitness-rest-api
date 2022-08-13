96b35623e43d8c602b28430b0a59d22d94ab94c7
Godwin254 <godwinbee15@gmail.com>
2022-08-13 18:31:41 +0300

    add error handlers at the data layer

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -4,35 +4,40 @@ const { saveToDatabase } = require('../database/utils');
 
 //accessing all workout data
 const getAllWorkouts = ( ) => {
-    return DB.workouts;
+    try{
+        return DB.workouts;
+    }catch(error){
+        throw {status: 500, message: error}
+    }
 }
 
 //accessing a single workout
 const getSingleWorkout = (workoutId) => {
-    const workout = DB.workouts.find(workout => workout.id === workoutId);
-
-    if (!workout) {
-        return; //handle later
+    try{
+        const workout = DB.workouts.find(workout => workout.id === workoutId);
+        if (!workout) {
+            return; //handle later
+        }
+        return workout;
+    }catch(error){
+        throw {status: error?.status, message: error?.message || error};
     }
-
-    return workout;
 }
 
 //create new workout to the DB
 const createNewWorkout = (newWorkout) => {
     
-    //check if already added
-    const workoutExists = DB.workouts.findIndex(workout => workout.name === newWorkout.name) > -1;
-
-    if (workoutExists) {
-        throw {
-            status: 400,
-            message: `Workout with name ${newWorkout.name} already exists`
-        };
-        //return;
-    }
-
     try{
+        //check if already added
+        const workoutExists = DB.workouts.findIndex(workout => workout.name === newWorkout.name) > -1;
+
+        if (workoutExists) {
+            throw {
+                status: 400,
+                message: `Workout with name ${newWorkout.name} already exists`
+            };
+            //return;
+        }
         DB.workouts.push(newWorkout);
         saveToDatabase(DB);
         return newWorkout;
@@ -46,44 +51,57 @@ const createNewWorkout = (newWorkout) => {
 
 //update a single workout
 const updateSingleWorkout = (workoutId, changes) => {
-    //check if index exists
-    const indexForUpdate = DB.workouts.findIndex(workout => workout.id === workoutId);
+    try{
+        //check if index exists
+        const indexForUpdate = DB.workouts.findIndex(workout => workout.id === workoutId);
 
-    //if index doesn't exist -1 
-    if (indexForUpdate === -1) {
-        throw new Error('Workout does not exist');
-        return;
+        //if index doesn't exist -1 
+        if (indexForUpdate === -1) {
+            throw new Error('Workout does not exist');
+            return;
+        }
+
+        //create object of the updated workout
+        const updatedWorkout = {
+            ...DB.workouts[indexForUpdate],
+            ...changes,
+            updatedAt: new Date().toDateString("en-US", { timeZone: "Africa/Nairobi" })
+        };
+
+        DB.workouts[indexForUpdate] = updatedWorkout;
+
+        saveToDatabase(DB);
+        return updatedWorkout
+    }catch(error){
+        throw {
+            status: error?.status || 500,
+            message: error?.message || error
+        };
     }
-
-    //create object of the updated workout
-    const updatedWorkout = {
-        ...DB.workouts[indexForUpdate],
-        ...changes,
-        updatedAt: new Date().toDateString("en-US", {timeZone: "Africa/Nairobi"})
-    };
-
-    DB.workouts[indexForUpdate] = updatedWorkout;
-
-    saveToDatabase(DB);
-    return updatedWorkout
 
 }
 
 // delete a single workout
 const deleteSingleWorkout = (workoutId) => {
-    //check if index exists
-    const indexForDelete = DB.workouts.findIndex(workout => workout.id === workoutId);
+    try{
+        //check if index exists
+        const indexForDelete = DB.workouts.findIndex(workout => workout.id === workoutId);
 
-    //if index doesn't exist -1 
-    if (indexForDelete === -1) {
-        throw new Error('Workout does not exist');
-        return;
+        //if index doesn't exist -1 
+        if (indexForDelete === -1) {
+            throw new Error('Workout does not exist');
+            return;
+        }
+
+        DB.workouts.splice(indexForDelete, 1);
+
+        saveToDatabase(DB);
+    }catch(error){
+        throw {
+            status: error?.status || 500,
+            message: error?.message || error
+        };
     }
-
-    DB.workouts.splice(indexForDelete, 1);
-
-    saveToDatabase(DB);
-    return;
 }
 
 module.exports = { 
